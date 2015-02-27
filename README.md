c0361036507a1983ea6a867bedbbbfb367d6490c
Viktor <viktor@nmmlm.com>
2015-02-27 16:23:14 +0200

    Simple maven project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sample_java_spring_app
 Initial java project with Spring.
+
+	## maven
+	$ mvn clean install
---
+++ src/main/java/com/flancer32/sample/App.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.flancer32.sample;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		}
+
+}
